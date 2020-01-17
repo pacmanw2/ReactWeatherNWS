33538c9d266fb3bd341cf5e7843bf2acd79b6971
Marco Gallegos <marco@ag.tools>
2020-01-17 00:21:08 -0800

    Hourly Forecast component
    Each forecast card is a child component of CardHourlyForecast.

---
+++ src/CardHourlyChild.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class CardHourlyChild extends React.Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                88 F
+                <div>4pm</div>
+            </div>
+        )
+    }
+}
+
+export default CardHourlyChild
