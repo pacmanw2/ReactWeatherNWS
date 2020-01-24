c7922d590decc6ceb7c6c0310e93e2284f51fbcb
Marco Gallegos <marco@ag.tools>
2020-01-24 03:03:31 -0800

    Weather Alert
    The weather alert now opens up a modal with tabs. Only the description tab currently works. The instruction tab will come next.

--- src/WeatherAlert.js
+++ src/WeatherAlert.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ReactModal from "react-modal";
+import ReactModal from 'react-modal';
 
 class WeatherAlert extends React.Component {
     constructor() {
         super()
         this.state = {
-            showModal: false
+            showModal: false,
+            nwsHeadline: 'N/A',
+            event: 'N/A',
+            description: 'N/A',
+            headline: 'N/A',
+            instruction: 'N/A'
         }
 
         this.handleOpenModal = this.handleOpenModal.bind(this)
         this.handleCloseModal = this.handleCloseModal.bind(this)
+
+        this.infoStyles = { 'backgroundColor': '#f09000', 'color': 'black' }
+        this.styles = { 'display': 'none', 'width': '40%' }
+    }
+
+    componentDidMount() {
+        ReactModal.setAppElement('#alertContainer');
+        this.getAlert();
     }
 
     handleOpenModal() {
@@ -20,39 +33,73 @@ class WeatherAlert extends React.Component {
         this.setState({ showModal: false });
     }
 
+    getAlert() {
+        var coor = "31,-88"
+        var url = "https://api.weather.gov/alerts/active?point=" + coor
+
+        fetch(url).then(
+            (response) => { return response.json() }
+        ).then(
+            (jsonObj) => {
+                console.log(jsonObj)
+                let properties = jsonObj.features[0].properties
+                let nwsHeadline = properties.parameters.NWSheadline
+                let event = properties.event
+                let headline = properties.headline
+                let description = properties.description
+                let instruction = properties.instruction
+                this.setState({
+                    nwsHeadline: nwsHeadline[0],
+                    event: event,
+                    description: description,
+                    headline: headline,
+                    instruction: instruction
+                })
+
+            }
+        )
+    }
+
+
     render() {
         return (
-            <div className="weather-alert-container">
+            <div className="weather-alert-container" id='alertContainer'>
                 <div className="weather-alert">
                     <button className="button is-rounded is-warning" onClick={this.handleOpenModal}>
                         View Alert(s)
-                </button>
+                    </button>
                     <ReactModal
                         isOpen={this.state.showModal}
                         contentLabel="Minimal Modal Example"
-                        className="modal-card"
                     >
+                        <div className="w3-bar w3-border w3-black w3-center">
+                            <button className="w3-bar-item w3-button" style={{ "width": "50%" }} onclick="openCity('London')">Description</button>
+                            <button className="w3-bar-item w3-button" style={{ "width": "50%" }} onclick="openCity('Paris')">Instruction</button>
+                        </div>
 
+                        <div id="London">
+                            <h2 style={{ "textAlign": "center" }}>{this.state.event}</h2>
+                            <p style={{ "textAlign": "center" }}>
+                                {this.state.description}
+                            </p>
+                        </div>
 
+                        <div id="Paris" className="w3-container city" style={this.styles}>
+                            <h2>Paris</h2>
+                            <p>Paris is the capital of France.</p>
+                        </div>
 
-                        <header className="modal-card-head">
-                            <p className="modal-card-title">Weather Alert</p>
+                        <div style={{ "display": "flex", "justifyContent": "center" }}>
+                            <button
+                                className="button is-warning is-rounded"
+                                onClick={this.handleCloseModal}>
+                                Close
+                            </button>
+                        </div>
 
-                        </header>
-                        <section className="modal-card-body">
-                            some stuff here and more here
-                            </section>
-                        <footer className="modal-card-foot">
-                            <button className="button" onClick={this.handleCloseModal}>Cancel</button>
-                        </footer>
-
-
-
-
-                        {/* <button onClick={this.handleCloseModal}>Close Modal</button> */}
                     </ReactModal>
                 </div>
-            </div>
+            </div >
         );
     }
 }
