935e9b3a0a79a3aec2957681aa190fac9286728e
Marco Gallegos <marco@ag.tools>
2020-02-03 23:51:51 -0800

    Commented out Tab.js
    commented out Tab.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import Card from './Card.js';
 import CardHourlyForecast from './CardHourlyForecast.js'
-import Tab from './Tab.js'
+// import Tab from './Tab.js'
 import './CardHourlyForecast.css';
 import './CardDailyForecast.js';
 import CardDailyForecast from './CardDailyForecast';
