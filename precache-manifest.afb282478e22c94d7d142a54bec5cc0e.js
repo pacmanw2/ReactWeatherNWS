34a9bdba48adee458de7162bf540220cdd26e75e
Marco Gallegos <marco@ag.tools>
2020-05-06 01:39:27 -0700

    Updates

---
+++ precache-manifest.afb282478e22c94d7d142a54bec5cc0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66747e3a44e1b005c50aacbe18177ed",
+    "url": "/ReactWeatherNWS/index.html"
+  },
+  {
+    "revision": "628543a5ddb6fff5a1d6",
+    "url": "/ReactWeatherNWS/static/css/main.7823c590.chunk.css"
+  },
+  {
+    "revision": "4e77d3f149f15e0f9b58",
+    "url": "/ReactWeatherNWS/static/js/2.19d2d1f9.chunk.js"
+  },
+  {
+    "revision": "24b5e7be899690b98b65800cb7aeb7ca",
+    "url": "/ReactWeatherNWS/static/js/2.19d2d1f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "628543a5ddb6fff5a1d6",
+    "url": "/ReactWeatherNWS/static/js/main.baa41d34.chunk.js"
+  },
+  {
+    "revision": "697885918ac126c4d433",
+    "url": "/ReactWeatherNWS/static/js/runtime-main.6af8f102.js"
+  }
+]);
